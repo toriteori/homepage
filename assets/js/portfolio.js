75bbfc1ac8463beed4770e9f3f140641bbcb2420
Seongmo Son <seongtori1029@gmail.com>
2024-03-06 18:57:36 +0900

    포트폴리오 js 함수 추가

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,3 +1,12 @@
+function setScreenSize() {
+    //먼저 뷰포트 높이를 얻고 1%를 곱하여 vh 단위 값을 얻습니다.
+    let vh = window.innerHeight * 0.01;
+    //그런 다음 --vh 사용자 정의 속성의 값을 문서의 루트로 설정합니다.
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
+
+
 // 커서 이벤트 함수
 function followCursor (){
     const cursorIcon = document.querySelector('#cursor');
@@ -339,6 +348,7 @@ function setMarquee (selector) {
 }
 
 document.addEventListener('DOMContentLoaded', function(){
+    setScreenSize();
     followCursor();
     enterEvent();
     commTab();
