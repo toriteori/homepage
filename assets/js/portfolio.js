d8881a6409dee2d35bed2799726e19266fd6a5bf
Seongmo Son <seongtori1029@gmail.com>
2024-03-07 13:10:53 +0900

    portfilio.js 수정

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -2,8 +2,11 @@ function setScreenSize() {
     //먼저 뷰포트 높이를 얻고 1%를 곱하여 vh 단위 값을 얻습니다.
     let vh = window.innerHeight * 0.01;
     //그런 다음 --vh 사용자 정의 속성의 값을 문서의 루트로 설정합니다.
-    let wrapper = document.querySelector('.wrapper');
     document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+    window.addEventListener('resize', function () {
+        setScreenSize();
+    })
 }
 
 
